c298b2eeb785e8fe7c453c686b10e2c9e5bb12ce
André Liberato <andre.liberato@outlook.com.br>
2023-10-09 17:58:52 -0300

    Adicionado cores personalizadas baseada na logo (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ export default {
     "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'app': {
+          'green': {
+            light: '#B0F291',
+            medium: '#04BF68',
+            dark: '#038C4C',
+            vivid: '#0DF205'
+          }
+        }
+      },
+    },
   },
   darkMode: "class",
   plugins: [require("tw-elements/dist/plugin.cjs")],
