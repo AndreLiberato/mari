f00022a8f9b13106ffac7e5e5dbfcaf339fa9aa4
victorbxavier <victorxavier530@gmail.com>
2023-10-09 21:48:16 -0300

    Adição do cadastro de prescritor

--- resources/views/cadastroPaciente.blade.php
+++ resources/views/cadastroPaciente.blade.php
@@ -343,12 +343,13 @@ require('@tailwindcss/forms'),
                                         </div>
                                     </div>
 
-                                    <div class="sm:col-span-3">
-                                     <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">Sintomas</label>
-                                        <div class="mt-2">
-                                            <input type="text" name="first-name" id="first-name" autocomplete="given-name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                                        </div>
+                                    <div class="col-span-full"> 
+                                    <label for="about" class="block text-sm font-medium leading-6 text-gray-900">Sintomas</label>
+                                    <div class="mt-2">
+                                        <textarea id="about" name="about" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
                                     </div>
+                                    </div>
+
                          </div>
                     </div>
                     
