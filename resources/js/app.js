e1e563918d138b672656c564626ac6a1ba7942a9
Gabriel Estácio <gabrielestaciosp@gmail.com>
2023-10-10 10:53:51 -0300

    Commit da tela de usuário

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,21 @@
 import './bootstrap';
 import { Input, initTE } from "tw-elements";
 initTE({ Input });
+import {
+    Collapse,
+    Dropdown,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Collapse, Dropdown });
+import {
+    Sidenav,
+    initTE,
+} from "tw-elements";  
+initTE({ Sidenav });
+import {
+    Ripple,
+    initTE,
+  } from "tw-elements";
+  
+  initTE({ Ripple });
