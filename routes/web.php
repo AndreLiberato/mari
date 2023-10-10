c3e2ed0278de7c137353c79bedbb2cfe6872d539
victorbxavier <victorxavier530@gmail.com>
2023-10-09 21:46:55 -0300

    Adição da rota de cadastro de prescritor

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/cadastro', function () {
+Route::get('/cadastroPaciente', function () {
     return view('cadastroPaciente');
+});
+
+Route::get('/cadastroPrescritor', function () {
+    return view('cadastroPrescritor');
 });
