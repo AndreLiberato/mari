f15ea4e8190e307263b726087592374db9db89a7
André Liberato <andre.liberato@outlook.com.br>
2023-12-07 09:01:26 -0300

    Proxy

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite([asset('build/assets/app-0e323167.css'), asset('build/assets/app-e4e59648.js')])
+    @vite(['resources/css/app.css','resources/js/app.js'])
     <title>{{ $title ?? 'Marijuana' }}</title>
 </head>
 <body>
